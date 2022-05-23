d7142d128324f58f850cfcb411b7de466c9e1da3
nataliep769 <nataliep769@gmail.com>
2022-05-22 21:38:25 -0600

    Added simple tests for controller

--- java_project/src/test/java/com/hingehealth/demo/controllers/BaseControllerTest.java
+++ java_project/src/test/java/com/hingehealth/demo/controllers/BaseControllerTest.java
@@ -1,12 +1,20 @@
 package com.hingehealth.demo.controllers;
 
+import com.hingehealth.demo.models.NodeRequest;
 import com.hingehealth.demo.services.TreeService;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.HashMap;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
+@ExtendWith(MockitoExtension.class)
 class BaseControllerTest {
 
     @Mock
@@ -19,5 +27,31 @@ class BaseControllerTest {
     void testHelloWorld() {
         String responseContents = baseController.helloWorld();
         assertEquals("Hello World!", responseContents);
+    }
+
+    @Test
+    void getTree() throws Exception {
+        // GIVEN
+        when(treeService.getTree()).thenReturn(new HashMap<>());
+
+        // WHEN
+        baseController.getTree();
+
+        // THEN
+        verify(treeService).getTree();
+    }
+
+    @Test
+    void addToTree() throws Exception {
+        // GIVEN
+        NodeRequest nodeRequest = new NodeRequest(7, "special node");
+        when(treeService.addNodeToTree(nodeRequest)).thenReturn("You successfully added a new node to your tree!");
+
+        // WHEN
+        String response = baseController.addNodeToTree(nodeRequest);
+
+        // THEN
+        verify(treeService).addNodeToTree(nodeRequest);
+        assertEquals("You successfully added a new node to your tree!", response);
     }
 }
